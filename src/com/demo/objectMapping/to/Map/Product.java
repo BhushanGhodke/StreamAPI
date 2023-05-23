c50575100baa6d92fc8ef4c0ea4bd652980eee64
BhushanGhodke <bhushanghodke1500@gmail.com>
2023-05-23 19:07:04 +0530

    Product Class Created

---
+++ src/com/demo/objectMapping/to/Map/Product.java
@@ -0,0 +1,39 @@
+package com.demo.objectMapping.to.Map;
+
+public class Product {
+
+	int productId;
+	String productName;
+	int price;
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", price=" + price + "]";
+	}
+	public Product(int productId, String productName, int price) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.price = price;
+	}
+	
+	
+
+}
